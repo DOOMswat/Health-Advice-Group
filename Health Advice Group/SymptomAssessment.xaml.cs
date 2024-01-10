4c6a65680e5f103aa9156e86807048f978235ba5
Jason Lac <Jasonlac2005@gmail.com>
2024-01-10 10:10:32 +0000

    -Updated symptom assessment windows. -Some validation on register window

--- Health Advice Group/SymptomAssessment.xaml.cs
+++ Health Advice Group/SymptomAssessment.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using MySql.Data.MySqlClient;
 namespace Health_Advice_Group
 {
     /// <summary>
@@ -24,6 +24,30 @@ namespace Health_Advice_Group
             InitializeComponent();
         }
 
+        private void btn_moreInfo_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(session.connStr))
+                {
+                    string selectedItem = ListBoxSymptoms.SelectedValue.ToString();
+                    conn.Open();
+                    string query = $"SELECT description FROM healthconditions Where conditionName = '{selectedItem}'";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        using (MySqlDataReader rdr = cmd.ExecuteReader())
+                        {
+                            if (rdr.Read())
+                            {
+                                string desc = rdr.GetString(0);
+                                txt_information.Text = desc;
+                            }
+                        }
+                    }
 
+                }
+            }
+            catch { MessageBox.Show("Error"); }
+        }   
     }
 }
--- Health Advice Group/registerWindow.xaml.cs
+++ Health Advice Group/registerWindow.xaml.cs
@@ -25,37 +25,48 @@ namespace Health_Advice_Group
             InitializeComponent();
         }
 
-        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        private void btnRegister_Click(object sender, RoutedEventArgs e)//Add validation
         {
             string password = txt_Password.Password;
             string conpass = txt_ConfirmPassword.Password;
             if (password == conpass)
             {
-                using (MySqlConnection conn = new MySqlConnection(session.connStr))
+                if (txt_Username.Text == null || txt_Surname.Text == null || txt_Forename.Text == null || txt_Email.Text == null || txt_Password == null || txt_ConfirmPassword == null)
                 {
-                    conn.Open();
-                    string query = "SELECT COUNT(*) FROM customer WHERE username = @userName";
-                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
-                    {//used to check if the username exists in the database or not
-                        cmd.Parameters.AddWithValue("@userName", txt_Username.Text.ToUpper());
-                        int userCount = Convert.ToInt32(cmd.ExecuteScalar());
-                        if (userCount > 0){MessageBox.Show("Username already exists.");return;}
+
+                    using (MySqlConnection conn = new MySqlConnection(session.connStr))
+                    {
+                        conn.Open();
+                        string query = "SELECT COUNT(*) FROM customer WHERE username = @userName";
+                        using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                        {//used to check if the username exists in the database or not
+                            cmd.Parameters.AddWithValue("@userName", txt_Username.Text.ToUpper());
+                            int userCount = Convert.ToInt32(cmd.ExecuteScalar());
+                            if (userCount > 0) { MessageBox.Show("Username already exists."); return; }
+                        }
+
+                        string insertCustomerQuery = "INSERT INTO customer (username, " +
+                            "firstName, lastName, email, password) " +
+                            "VALUES(@userName,@firstName,@lastName,@emailAddress,SHA(@passWord));";
+                        MySqlCommand insertCustomerCommand = new MySqlCommand(@insertCustomerQuery, conn);
+                        insertCustomerCommand.Parameters.AddWithValue("@userName", txt_Username.Text.ToUpper());
+                        insertCustomerCommand.Parameters.AddWithValue("@firstName", txt_Forename.Text);
+                        insertCustomerCommand.Parameters.AddWithValue("@lastName", txt_Surname.Text);
+                        insertCustomerCommand.Parameters.AddWithValue("@emailAddress", txt_Email.Text);
+                        insertCustomerCommand.Parameters.AddWithValue("@passWord", txt_ConfirmPassword.Password);
+                        insertCustomerCommand.ExecuteNonQuery();
+                        MessageBox.Show("Account sucessfully created");
+                        LoginWindow loginWindow = new LoginWindow();
+                        loginWindow.Show();
+                        this.Close();
                     }
 
-                    string insertCustomerQuery = "INSERT INTO customer (username, " +
-                        "firstName, lastName, email, password) " +
-                        "VALUES(@userName,@firstName,@lastName,@emailAddress,SHA(@passWord));";
-                    MySqlCommand insertCustomerCommand = new MySqlCommand(@insertCustomerQuery, conn);
-                    insertCustomerCommand.Parameters.AddWithValue("@userName", txt_Username.Text.ToUpper());
-                    insertCustomerCommand.Parameters.AddWithValue("@firstName",txt_Forename.Text);
-                    insertCustomerCommand.Parameters.AddWithValue("@lastName",txt_Surname.Text);
-                    insertCustomerCommand.Parameters.AddWithValue("@emailAddress", txt_Email.Text);
-                    insertCustomerCommand.Parameters.AddWithValue("@passWord", txt_ConfirmPassword.Password);
-                    insertCustomerCommand.ExecuteNonQuery();
-                    MessageBox.Show("Account sucessfully created");
-                    LoginWindow loginWindow = new LoginWindow();
-                    loginWindow.Show();
-                    this.Close();
+
+                }
+                
+                else
+                {
+                    MessageBox.Show("Please fill in all the boxes.");
                 }
 
 
